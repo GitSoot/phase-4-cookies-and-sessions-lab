cef5c9c547d956922db0ed540514a0c0c4fa26ca
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-03-23 05:59:17 +0300

    Assignment completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
-class ApplicationController < ActionController::API
-  include ActionController::Cookies
+class ApplicationController < ActionController::Base
+  before_action :set_page_views
 
+  private
+
+  def set_page_views
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    if session[:page_views] > 3
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
+  end
 end
